61bc0b44d373d20b918f234ea206082c515a18f3
Felipe <felipe03012007@gmail.com>
2025-02-11 15:00:16 -0300

    Mudando title

--- index (1).php
+++ index (1).php
@@ -2,7 +2,7 @@
     <head>
         
 		
-		<title>HelpVision</title>
+		<title>Help Vision</title>
        <meta charset="utf-8">
        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
